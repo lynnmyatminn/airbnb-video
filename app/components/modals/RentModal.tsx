acf23aaa8186d03682da5878e277a2c4ac613025
linmyatmin <lynnmyatminn@gmail.com>
2023-05-30 10:22:36 +0700

    Counter Components

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -7,7 +7,8 @@ import { categories } from '../navbar/Categories';
 import CategoryInput from '../inputs/CategoryInput';
 import { FieldValues, useForm } from 'react-hook-form';
 import CountrySelect from '../inputs/CountrySelect';
-import Map from '../Map';
+import dynamic from 'next/dynamic';
+import Counter from '../inputs/Counter';
 
 enum STEPS {
   CATEGORY = 0,
@@ -45,6 +46,17 @@ const RentModal = () => {
 
   const category = watch('category');
   const location = watch('location');
+  const guestCount = watch('guestCount');
+  const roomCount = watch('roomCount');
+  const bathroomCount = watch('bathroomCount');
+
+  const Map = useMemo(
+    () =>
+      dynamic(() => import('../Map'), {
+        ssr: false,
+      }),
+    [location]
+  );
 
   const setCustomValue = (id: string, value: any) => {
     setValue(id, value, {
@@ -75,14 +87,14 @@ const RentModal = () => {
   }, [step]);
 
   let bodyContent = (
-    <div className="flex flex-col gap-8">
+    <div className='flex flex-col gap-8'>
       <Heading
-        title="Which of these best describes your place?"
-        subtitle="Pick a category"
+        title='Which of these best describes your place?'
+        subtitle='Pick a category'
       />
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-3 max-h-[50vh] overflow-y-auto">
+      <div className='grid grid-cols-1 md:grid-cols-2 gap-3 max-h-[50vh] overflow-y-auto'>
         {categories.map((item) => (
-          <div key={item.label} className="col-span-1">
+          <div key={item.label} className='col-span-1'>
             <CategoryInput
               onClick={(category) => setCustomValue('category', category)}
               selected={category === item.label}
@@ -98,22 +110,53 @@ const RentModal = () => {
   if (step === STEPS.LOCATION) {
     bodyContent = (
       <div
-        className="
+        className='
     flex
     flex-col
     gap-8
-    "
+    '
       >
         <Heading
-          title="Where is your place located?"
-          subtitle="Help guests find you!"
+          title='Where is your place located?'
+          subtitle='Help guests find you!'
         />
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-3 max-h-[50vh] overflow-y-auto"></div>
+        <div className='grid grid-cols-1 md:grid-cols-2 gap-3 max-h-[50vh] overflow-y-auto'></div>
         <CountrySelect
           value={location}
           onChange={(value) => setCustomValue('location', value)}
         />
-        <Map />
+        <Map center={location?.latlng} />
+      </div>
+    );
+  }
+
+  if (step === STEPS.INFO) {
+    bodyContent = (
+      <div className='flex flex-col gap-8'>
+        <Heading
+          title='Share some basics about your place'
+          subtitle='What amenities do you have?'
+        />
+        <Counter
+          title='Guests'
+          subtitle='How many guests do you allow?'
+          value={guestCount}
+          onChange={(value) => setCustomValue('guestCount', value)}
+        />
+        <hr />
+        <Counter
+          title='Rooms'
+          subtitle='How many rooms do you have?'
+          value={roomCount}
+          onChange={(value) => setCustomValue('roomCount', value)}
+        />
+        <hr />
+        <Counter
+          title='Bathrooms'
+          subtitle='How many bathrooms do you have?'
+          value={bathroomCount}
+          onChange={(value) => setCustomValue('bathroomCount', value)}
+        />
       </div>
     );
   }
@@ -126,7 +169,7 @@ const RentModal = () => {
       actionLabel={actionLabel}
       secondaryActionLabel={secondaryActionLabel}
       secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
-      title="Airbnb your home!"
+      title='Airbnb your home!'
       body={bodyContent}
     />
   );
